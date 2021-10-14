b636e5da51691f674fe4d3de136eec76995e68f4
KaseyColeman <kcoleman@student.neumont.edu>
2021-10-14 16:22:31 -0600

    updated a lot of stuff

--- js/Backend/database.js
+++ js/Backend/database.js
@@ -1,12 +1,17 @@
-const { MongoClient } = require('mongodb');
-
-const uri = "mongodb+srv://KaseyColeman:KaseyColeman@convalesce.wql1e.mongodb.net/UserInfo?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-client.connect(err => {
-    const collection = client.db("UserInfo").collection("Journals");
-    // perform actions on the collection object
-    client.close();
-});
-
-
+const { MongoClient };
+ 
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = "mongodb+srv://KaseyColeman:KaseyColeman@convalesce.wql1e.mongodb.net/UserInfo?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
