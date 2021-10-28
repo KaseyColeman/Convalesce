43baf077737341c3a7539c1e885610d2c0aa53f6
KaseyColeman <kcoleman@student.neumont.edu>
2021-10-28 14:43:15 -0600

    Refactored Login

--- js/Backend/index.js
+++ js/Backend/index.js
@@ -1,27 +1,27 @@
-let currentData = null
+// let currentData = null
 
-fetch('http://localhost:8080/health')
-    .then(response => response.json())
-    .then(data => currentData=data);
+// fetch('http://localhost:8080/health')
+//     .then(response => response.json())
+//     .then(data => currentData=data);
 
-const lname =  document.getElementById("uname1");
-const lpass = document.getElementById("psw1");
+// const lname =  document.getElementById("uname1");
+// const lpass = document.getElementById("psw1");
 
-lname.onchange = function (){
-    console.log("wee");
-    console.log(lname.value);
-    console.log(currentData);
-}
+// lname.onchange = function (){
+//     console.log("wee");
+//     console.log(lname.value);
+//     console.log(currentData);
+// }
 
-function login(){
-    console.log(lname.value);
-    console.log(lpass.value);
+// function login(){
+//     console.log(lname.value);
+//     console.log(lpass.value);
 
-    if(lname.value == "kcoleman@student.neumont.edu"){
-        window.location.replace = "http://127.0.0.1:5500/patient-dashboard.html"; 
-    }
-    window.location.replace = "http://127.0.0.1:5500/index.html"; 
-}
+//     if(lname.value == "kcoleman@student.neumont.edu"){
+//         window.location.replace = "http://127.0.0.1:5500/patient-dashboard.html"; 
+//     }
+//     window.location.replace = "http://127.0.0.1:5500/index.html"; 
+// }
 
 // function validate(){
 //     var username = document.getElementById("username").value;
